3d7cb73fe75b0058b3149954852fc187cba20bb3
prajwalc150 <prajwalmech8@gmail.com>
2022-09-21 20:21:04 +0530

    Add operate() function

--- app.js
+++ app.js
@@ -16,3 +16,29 @@ function divide(a,b){
     return a/b;
 }
 
+//create operate()
+
+function operate(a,b,c)
+{
+    /*switch((a)){
+        case '+':
+            add(b,c);
+            break;
+        case '-':
+            subtract(b,c);
+            break;
+        case '* ':
+            multiply(b,c);
+            break;
+        case '/':
+            divide(b,c);
+            break;
+    }*/
+
+    if(a=="+"){return add(b,c);}
+    if(a=="-"){return subtract(b,c);}
+    if(a=="*"){return multiply(b,c);}
+    if(a=="/"){return divide(b,c);}
+}
+//console.log(operate('-',3,4));
+//buttons, display, and clear button.
