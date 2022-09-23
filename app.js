25725cad714cb9127e15800755687a231d12c39a
prajwalc150 <prajwalmech8@gmail.com>
2022-09-23 22:08:39 +0530

    Add buttons

--- app.js
+++ app.js
@@ -41,3 +41,23 @@ function operate(a,b,c)
     if(a=="/"){return divide(b,c);}
 }
 //console.log(operate('-',3,4));
+
+const container = document.querySelector('.dig1')
+for(i = 2;i>-1;i--){
+    //m = 800/n;
+    let content = document.createElement('div');
+    content.id = `digRow${i}`;
+    //content.classList.add("test");
+    container.appendChild(content);
+    //document.getElementById(`test${i}`).style.height = `${m}px`;
+    for( j = 1;j<4;j++){
+      const container2 = document.querySelector(`#digRow${i}`);
+      let content2 = document.createElement('button');
+      content2.id= `jest${i},${j}`;
+      content2.classList.add("jest");
+      content2.textContent = i*3+j;
+      container2.appendChild(content2);
+      //document.getElementById(`jest${i},${j}`).style.width = `${m}px`;
+      
+    } 
+  }
